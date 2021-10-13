cb346e4de158ae189bcc548d7fad9f9b8a4642bc
EstebanAlarconO <esteban.alarcon1@mail.udp.cl>
2021-10-12 21:11:15 -0300

    cambio menor

--- hash.py
+++ hash.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
                     print ("{:<10} {:<10}".format(entropia(texto), texto))
 
         elif(opcion == '3'):
-            exit()
+            flag = False
 
         else:
             print('Debe ingresar una opcion valida\n')   
